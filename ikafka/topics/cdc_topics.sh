7941b34b16c34f6f706c57502c0e0829d38a7a2a
Sergei <sergei_777@tut.by>
2026-03-18 00:01:33 +0300

    Added script to create CDC Kafka topics with compact cleanup policy

---
+++ ikafka/topics/cdc_topics.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#<database>.<schema>.<table>
+
+BOOTSTRAP="kafka-0:9092"
+KAFKA="/opt/bitnami/kafka/bin/kafka-topics.sh"
+
+create_topic () {
+   $KAFKA \
+    --bootstrap-server $BOOTSTRAP \
+    --create \
+    --if-not-exists \
+    --topic $1 \
+    --partitions 3 \
+    --replication-factor 3 \
+    --config cleanup.policy=compact
+}
+
+create_topic customers.public.users
+create_topic customers.public.orders
